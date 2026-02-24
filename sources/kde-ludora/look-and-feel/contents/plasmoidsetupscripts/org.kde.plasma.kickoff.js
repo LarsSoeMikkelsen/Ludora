db91ac57d0db5fa2c8059e72be2df94749a89b7b
LarsSoeMikkelsen <larssoemikkelsen@gmail.com>
2026-02-24 23:25:29 +0100

    Delete sources/kde-ludora directory

--- sources/kde-ludora/look-and-feel/contents/plasmoidsetupscripts/org.kde.plasma.kickoff.js
+++
@@ -1,9 +0,0 @@
-/*
-   Set launcher icon to ludora-start-here
-   Based on Fedora's kickoff icon setup script
-*/
-
-if ( applet.readConfig("icon", "start-here-kde") == "start-here-kde" ) {
-  applet.currentConfigGroup = ["General"]
-  applet.writeConfig("icon", "ludora-start-here");
-}
