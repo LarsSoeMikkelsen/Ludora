be4b4503748acb50dbac03bb2f90746e13c9f88e
LarsSoeMikkelsen <larssoemikkelsen@gmail.com>
2026-02-24 23:26:02 +0100

    Add files via upload

---
+++ sources/kde-ludora/look-and-feel/contents/plasmoidsetupscripts/org.kde.plasma.kickerdash.js
@@ -0,0 +1,9 @@
+/*
+   Set launcher icon to ludora-start-here
+   Based on Fedora's kickerdash icon setup script
+*/
+
+if ( applet.readConfig("icon", "start-here-kde") == "start-here-kde" ) {
+  applet.currentConfigGroup = ["General"]
+  applet.writeConfig("icon", "ludora-start-here");
+}
