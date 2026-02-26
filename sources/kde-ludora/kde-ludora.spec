d5afd38a83d56c81eda2e89fd622eb3bc3b286d3
LarsSoeMikkelsen <larssoemikkelsen@gmail.com>
2026-02-26 22:44:33 +0100

    Add files via upload

--- sources/kde-ludora/kde-ludora.spec
+++ sources/kde-ludora/kde-ludora.spec
@@ -1,28 +1,36 @@
 Name:           kde-ludora
-Version:        1.0
+Version:        1.1
 Release:        1%{?dist}
 Summary:        Ludora KDE Plasma Global Theme
 License:        GPLv2+
-URL:            https://github.com/LarsSoeMikkelsen/Ludora/tree/main/sources/kde-ludora
+URL:            https://ludora.org
 Source0:        kde-ludora-%{version}.tar.gz
 BuildArch:      noarch
 Requires:       plasma-workspace
 Requires:       papirus-icon-theme
+
 %description
-Ludora is a KDE Plasma global theme based on Breeze Dark with Papirus icons,
-featuring custom wallpapers and branding for the Ludora distribution.
+Ludora is a KDE Plasma global theme for the Ludora gaming Linux distribution.
+Based on Breeze Dark with a custom amber accent color scheme, centered taskbar,
+icons-only task manager, Papirus-Dark icons, and Ludora branding.
+
 %define debug_package %{nil}
+
 %prep
 %setup -T -b 0 -q -n kde-ludora-%{version}
+
 %build
 # Nothing to build
+
 %install
 # Install look-and-feel theme
 mkdir -p %{buildroot}%{_datadir}/plasma/look-and-feel/org.ludora.desktop
 cp -ar look-and-feel/* %{buildroot}%{_datadir}/plasma/look-and-feel/org.ludora.desktop/
-# Install wallpaper
-mkdir -p %{buildroot}%{_datadir}/wallpapers/Ludora
-cp -ar wallpaper/* %{buildroot}%{_datadir}/wallpapers/Ludora/
+
+# Install color scheme
+mkdir -p %{buildroot}%{_datadir}/color-schemes
+cp color-schemes/Ludora.colors %{buildroot}%{_datadir}/color-schemes/
+
 # Install icons
 mkdir -p %{buildroot}%{_datadir}/icons/hicolor/scalable/apps
 mkdir -p %{buildroot}%{_datadir}/icons/hicolor/64x64/apps
@@ -32,23 +40,34 @@ cp icons/scalable/ludora-start-here.svg %{buildroot}%{_datadir}/icons/hicolor/sc
 cp icons/64x64/ludora-start-here.png %{buildroot}%{_datadir}/icons/hicolor/64x64/apps/
 cp icons/128x128/ludora-start-here.svg %{buildroot}%{_datadir}/icons/hicolor/128x128/apps/
 cp icons/256x256/ludora-start-here.svg %{buildroot}%{_datadir}/icons/hicolor/256x256/apps/
+
 %files
 %{_datadir}/plasma/look-and-feel/org.ludora.desktop/
-%{_datadir}/wallpapers/Ludora/
+%{_datadir}/color-schemes/Ludora.colors
 %{_datadir}/icons/hicolor/scalable/apps/ludora-start-here.svg
 %{_datadir}/icons/hicolor/64x64/apps/ludora-start-here.png
 %{_datadir}/icons/hicolor/128x128/apps/ludora-start-here.svg
 %{_datadir}/icons/hicolor/256x256/apps/ludora-start-here.svg
+
 %post
 touch --no-create %{_datadir}/icons/hicolor &>/dev/null || :
+
 %postun
 if [ $1 -eq 0 ] ; then
     touch --no-create %{_datadir}/icons/hicolor &>/dev/null
     gtk-update-icon-cache %{_datadir}/icons/hicolor &>/dev/null || :
 fi
+
 %posttrans
 gtk-update-icon-cache %{_datadir}/icons/hicolor &>/dev/null || :
+
 %changelog
+* Thu Feb 26 2026 Ludora Team <team@ludora.org> - 1.1-1
+- Add custom Ludora amber color scheme replacing Breeze Dark blue accent
+- Update panel layout: centered taskbar, icons-only task manager, proper spacers
+- Fix kickoff launcher icon to use theme name instead of hardcoded path
+- Update metadata with website and description
+- Remove wallpaper package (wallpaper handled separately)
+
 * Mon Feb 24 2025 Ludora Team <team@ludora.org> - 1.0-1
 - Initial release of Ludora KDE theme
-- Uses Papirus-Dark icon theme
--- sources/kde-ludora/look-and-feel/contents/layouts/org.kde.plasma.desktop-layout.js
+++ sources/kde-ludora/look-and-feel/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -1,6 +1,111 @@
-loadTemplate("org.kde.plasma.desktop.defaultPanel")
+var plasma = getApiVersion(1);
 
-var desktopsArray = desktopsForActivity(currentActivity());
-for( var j = 0; j < desktopsArray.length; j++) {
-    desktopsArray[j].wallpaperPlugin = 'org.kde.image';
+var layout = {
+    "desktops": [
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "ItemGeometriesHorizontal": "",
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/Wallpaper/org.kde.image/General": {
+                    "FillMode": "6",
+                    "SlidePaths": "/usr/share/wallpapers/"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        }
+    ],
+    "panels": [
+        {
+            "alignment": "center",
+            "applets": [
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.panelspacer"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "popupHeight": "509",
+                            "popupWidth": "647"
+                        },
+                        "/General": {
+                            "favoritesPortedToKAstats": "true",
+                            "icon": "ludora-start-here",
+                            "systemFavorites": "suspend\\,hibernate\\,reboot\\,shutdown"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.kickoff"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.pager"
+                },
+                {
+                    "config": {
+                        "/General": {
+                            "groupedTaskVisualization": "2",
+                            "iconSpacing": "0",
+                            "launchers": "applications:org.kde.konsole.desktop,preferred://filemanager,preferred://browser,applications:steam.desktop"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.icontasks"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.panelspacer"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.marginsseparator"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.systemtray"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "popupHeight": "400",
+                            "popupWidth": "560"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.digitalclock"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.showdesktop"
+                }
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "2",
+                    "immutability": "1",
+                    "lastScreen": "0"
+                }
+            },
+            "height": 3.6666666666666665,
+            "hiding": "normal",
+            "location": "bottom",
+            "maximumLength": 191.11111111111111,
+            "minimumLength": 191.11111111111111,
+            "offset": 0
+        }
+    ],
+    "serializationFormatVersion": "1"
 }
+;
+
+plasma.loadSerializedLayout(layout);
